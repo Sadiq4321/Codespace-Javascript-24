1e8726d9bdc37c9794533a458bdf7f6c6ddb9e7c
Sadiq4321 <162881644+Sadiq4321@users.noreply.github.com>
2024-03-10 14:48:31 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1234;
+let accountEmail = "sadiq@google.com"
+var accountPassword = "123456"
+accountCity = "bengaluru" //not preferred but can be done
+let accountstate; //it will show undefined
+
+//prefer not use var because of issue//
+
+
+
+accountEmail = "sid@goodle.com"
+accountPassword = "432156"
+
+accountCity = "bangalore"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"//use all js code as newer version, nowadays it is by default
+
+alert (3 + 3) //run it wil show error because it should be binded with nodejs,
+//it wiil work in browser//
+
+/* mdn is written by mozilla, good, 
+but original documentation of javascript is in tc39.es/ecma262/#sec-intro*/
+
+//already learn
+
+//datatypes
+//symbol => to find unique
